856365618b8ee7471eed07a4945ea7eebb5008ca
César Guerra <warderer@gmail.com>
2024-01-24 19:59:42 -0600

    ✨ Componente Home Basico

---
+++ 02-react-context/src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <div className='izquierdo'>
+        <h2>SongList</h2>
+      </div>
+
+      <div className='derecho'>
+        <h2>SongDetail</h2>
+      </div>
+    </div>
+  )
+}
+export default Home
