ae7acf12d65765945fb72925c02da3c66e84d45c
CesarDev96 <cesar@crecy.io>
2024-01-31 21:02:22 -0600

    Creacion del formulario Login

--- 03-react-auth/src/pages/Login.jsx
+++ 03-react-auth/src/pages/Login.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import Logo from '@/assets/react.svg'
+import '@/styles/form.css'
 
 function Login() {
   return (
-    <div>Login</div>
+    <main className='form-signin w-100 m-auto'>
+      <form>
+        <img className='mb-4' src={Logo} alt='Icono de react' width={72} height={57}/>
+        <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+
+        <div className='form-floating'>
+          <input type='email' className='form-control' id='floatingInput' placeholder='name@example.com'/>
+          <label htmlFor='floatingInput'>Email address</label>
+        </div>
+        <div className='form-floating'>
+          <input type='password' className='form-control' id='floatingPassword' placeholder='Password'/>
+          <label htmlFor='floatingPassword'>Password</label>
+        </div>
+
+        <div className='form-check text-start my-3'>
+          <input className='form-check-input' type='checkbox' value='remember-me' id='flexCheckDefault'/>
+          <label className='form-check-label' htmlFor='flexCheckDefault'>
+            Remember me
+          </label>
+        </div>
+        <button className='btn btn-primary w-100 py-2' type='submit'>Sign in</button>
+        <p className='mt-5 mb-3 text-body-secondary'>© 2017–2023</p>
+      </form>
+    </main>
   )
 }
 
