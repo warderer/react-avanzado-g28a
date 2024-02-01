6e97973249f9fffd84971a072603b8ea24a529ea
CesarDev96 <cesar@crecy.io>
2024-01-31 21:16:46 -0600

    Implementacion de react-hook-form

--- 03-react-auth/src/pages/Login.jsx
+++ 03-react-auth/src/pages/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
     <main className='form-signin w-100 m-auto'>
       <form>
         <img className='mb-4' src={Logo} alt='Icono de react' width={72} height={57}/>
-        <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+        <h1 className='h3 mb-3 fw-normal'>Please login</h1>
 
         <div className='form-floating'>
           <input type='email' className='form-control' id='floatingInput' placeholder='name@example.com'/>
@@ -23,7 +23,7 @@ function Login() {
             Remember me
           </label>
         </div>
-        <button className='btn btn-primary w-100 py-2' type='submit'>Sign in</button>
+        <button className='btn btn-primary w-100 py-2' type='submit'>Login</button>
         <p className='mt-5 mb-3 text-body-secondary'>© 2017–2023</p>
       </form>
     </main>
--- 03-react-auth/src/pages/Signup.jsx
+++ 03-react-auth/src/pages/Signup.jsx
@@ -1,8 +1,88 @@
-import React from 'react'
+import { useForm } from 'react-hook-form'
+import Logo from '@/assets/react.svg'
 
 function Signup() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
   return (
-    <div>Signup</div>
+    <main className='form-signin w-100 m-auto'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <img className='mb-4' src={Logo} alt='' width='72' height='57' />
+        <h1 className='h3 mb-3 fw-normal'>Please sign up</h1>
+
+        <div className='form-floating'>
+          <input
+            type='text'
+            className='form-control'
+            id='first_name'
+            name='first_name'
+            placeholder='John'
+            {...register('first_name', { required: true })}
+          />
+          <label htmlFor='first_name'>First Name</label>
+        </div>
+
+        <div className='form-floating'>
+          <input
+            type='text'
+            className='form-control'
+            id='last_name'
+            name='last_name'
+            placeholder='Doe'
+            {...register('last_name', { required: true })}
+          />
+          <label htmlFor='last_name'>Last Name</label>
+        </div>
+
+        <div className='form-floating'>
+          <select
+            className='form-select'
+            id='gender'
+            name='gender'
+            {...register('gender', { required: true })}
+          >
+            <option value=''>Choose...</option>
+            <option value='M'>Male</option>
+            <option value='F'>Female</option>
+          </select>
+          <label htmlFor='gender'>Gender</label>
+        </div>
+
+        <div className='form-floating'>
+          <input
+            type='email'
+            className='form-control'
+            id='email'
+            name='email'
+            placeholder='name@example.com'
+            {...register('email', { required: true })}
+          />
+          <label htmlFor='email'>Email address</label>
+        </div>
+
+        <div className='form-floating'>
+          <input
+            type='password'
+            className='form-control'
+            id='password'
+            name='password'
+            placeholder='Password'
+            {...register('password', { required: true })}
+          />
+          <label htmlFor='password'>Password</label>
+        </div>
+
+        <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign up</button>
+        <p className='mt-5 mb-3 text-muted'>© 2017–2022</p>
+      </form>
+    </main>
   )
 }
 
