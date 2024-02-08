a817fa6263bc785453adb3946c6bf1e495dee583
César Guerra <warderer@gmail.com>
2024-02-07 19:01:43 -0600

    ✨ Login con react-hook-form

--- 03-react-auth/src/pages/Login.jsx
+++ 03-react-auth/src/pages/Login.jsx
@@ -1,28 +1,61 @@
+import { useNavigate } from 'react-router-dom'
+import { loginUserServices } from '@/services/useServices'
+import { useForm } from 'react-hook-form'
 import Logo from '@/assets/react.svg'
 import '@/styles/form.css'
 
-function Login() {
+function Login () {
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await loginUserServices(data)
+      if (response.status === 200) {
+        console.log('OK')
+        navigate('/dashboard')
+      }
+      console.log('response', response)
+    } catch (error) {
+      console.log('error', error.message)
+    }
+  }
+
   return (
     <main className='form-signin w-100 m-auto'>
-      <form>
-        <img className='mb-4' src={Logo} alt='Icono de react' width={72} height={57}/>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <img className='mb-4' src={Logo} alt='Icono de react' width={72} height={57} />
         <h1 className='h3 mb-3 fw-normal'>Please login</h1>
 
         <div className='form-floating'>
-          <input type='email' className='form-control' id='floatingInput' placeholder='name@example.com'/>
+          <input
+            type='email'
+            className='form-control'
+            id='floatingInput'
+            placeholder='name@example.com'
+            {...register('email', { required: true })}
+          />
           <label htmlFor='floatingInput'>Email address</label>
         </div>
+        {errors.email && <span className='text-danger'>This field is required</span>}
+
         <div className='form-floating'>
-          <input type='password' className='form-control' id='floatingPassword' placeholder='Password'/>
+          <input
+            type='password'
+            className='form-control'
+            id='floatingPassword'
+            placeholder='Password'
+            {...register('password', { required: true })}
+          />
           <label htmlFor='floatingPassword'>Password</label>
         </div>
+        {errors.password && <span className='text-danger'>This field is required</span>}
 
-        <div className='form-check text-start my-3'>
-          <input className='form-check-input' type='checkbox' value='remember-me' id='flexCheckDefault'/>
-          <label className='form-check-label' htmlFor='flexCheckDefault'>
-            Remember me
-          </label>
-        </div>
         <button className='btn btn-primary w-100 py-2' type='submit'>Login</button>
         <p className='mt-5 mb-3 text-body-secondary'>© 2017–2023</p>
       </form>
--- 03-react-auth/src/services/useServices.js
+++ 03-react-auth/src/services/useServices.js
@@ -3,5 +3,9 @@ import axios from 'axios'
 const BASE_URL = 'https://ecommerce-json-jwt.onrender.com'
 
 const registerUserServices = (data) => axios.post(`${BASE_URL}/register`, data)
+const loginUserServices = (data) => axios.post(`${BASE_URL}/login`, data)
 
-export { registerUserServices }
+export {
+  registerUserServices,
+  loginUserServices
+}
