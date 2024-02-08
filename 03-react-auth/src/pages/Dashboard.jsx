6a50b46f1109aba72b6ed8cbfd71af1c4f1d8a24
César Guerra <warderer@gmail.com>
2024-02-07 21:11:14 -0600

    ✨ Perfil de usuario en Dashboard

--- 03-react-auth/src/pages/Dashboard.jsx
+++ 03-react-auth/src/pages/Dashboard.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { getMeUserServices } from '@/services/useServices'
 
-function Dashboard() {
+function Dashboard () {
+  const [userData, setUserData] = useState({})
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    const getMeUser = async () => {
+      const response = await getMeUserServices(token)
+      if (response.status === 200) {
+        setUserData(response.data)
+      }
+    }
+    getMeUser()
+  }, [token])
+
   return (
-    <div>Dashboard</div>
+    <>
+      <h1>Dashboard</h1>
+      {userData?.first_name && <p>Nombre: {userData.first_name}</p>}
+      {userData?.last_name && <p>Apellido: {userData.last_name}</p>}
+      {userData?.gender && <p>Genero: {userData.gender}</p>}
+      {userData?.email && <p>Email: {userData.email}</p>}
+      {userData?.role && <p>Rol: {userData.role}</p>}
+    </>
   )
 }
 
--- 03-react-auth/src/services/useServices.js
+++ 03-react-auth/src/services/useServices.js
@@ -4,8 +4,14 @@ const BASE_URL = 'https://ecommerce-json-jwt.onrender.com'
 
 const registerUserServices = (data) => axios.post(`${BASE_URL}/register`, data)
 const loginUserServices = (data) => axios.post(`${BASE_URL}/login`, data)
+const getMeUserServices = (jwtToken) => axios.get(`${BASE_URL}/users/me`, {
+  headers: {
+    Authorization: `Bearer ${jwtToken}`
+  }
+})
 
 export {
   registerUserServices,
-  loginUserServices
+  loginUserServices,
+  getMeUserServices
 }
